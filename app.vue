43645a8d3e41f1118f351fa226f04e12e59c8c3a
LaetusH <henri@die-froehlichs.net>
2025-11-22 11:50:34 +0100

    feat: progressive webapp functionality

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MainApp from './components/MainApp.vue';
+
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'},
+    { name: 'apple-mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+    { name: 'apple-mobile-web-app-title', content: 'Tempute' }
+  ],
+  link: [
+    { rel: 'apple-touch-icon', href: '/icons/logo-192x192.png' }
+  ]
+})
+</script>
+<template>
+  <div>
+    <MainApp/>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  compatibilityDate: '2025-11-22',
+  devtools: { enabled: false },
+  ssr: false,
+  modules: ['@vite-pwa/nuxt', '@nuxtjs/leaflet'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  pages: false,
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Tempute',
+      short_name: 'Tempute',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait',
+      icons: [
+        {
+          src: '/logo-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/logo-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: '/logo-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      orsApiKey: process.env.ORS_API_KEY || ''
+    }
+  }
 })
